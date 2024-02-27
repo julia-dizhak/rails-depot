0e6d2aa86f1c676970d91bce159c5ad57ef69ab8
Julia Dizhak <julia.dizhak@teamtailor.com>
2024-02-27 19:59:22 +0100

    highlithing the changes

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -30,7 +30,7 @@ class LineItemsController < ApplicationController
     
     respond_to do |format|
       if @line_item.save
-        format.turbo_stream
+        format.turbo_stream { @current_item = @line_item }
         format.html { redirect_to store_index_url, notice: "Line item was successfully created."  }
         # format.html { redirect_to cart_url(@line_item.cart), notice: "Line item was successfully created." }
         format.json { render :show, status: :created, location: @line_item }
