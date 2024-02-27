940b0f775b5c4e1f7369840445c4ffa657aedab4
Julia Dizhak <julia.dizhak@teamtailor.com>
2024-02-27 23:38:01 +0100

    capturing the order details

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,7 +1,8 @@
 # This table represents line items in a shopping cart. 
 class LineItem < ApplicationRecord
+  belongs_to :order, optional: true
   belongs_to :product
-  belongs_to :cart
+  belongs_to :cart, optional: true
 
   def total_price
     product.price * quantity
