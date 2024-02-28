a37d0fe1081903bfbe55df9525e6ca12be13e1bf
Julia Dizhak <julia.dizhak@teamtailor.com>
2024-02-28 14:00:06 +0100

    add users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_27_200520) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_28_124800) do
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -42,6 +42,13 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_27_200520) do
     t.text "description"
     t.string "image_url"
     t.decimal "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
